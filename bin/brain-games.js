155eaa59da2e11bfdff5095b0a3194eae6074f91
Quiet_Death <familuuuuudamira@gmail.com>
2025-02-27 11:04:29 +0300

    2
    2 задача

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,5 @@
-console.log ('Weclome to the Brain Games!')
+console.log ('Weclome to the Brain Games!');
+import readlineSync from 'readline-sync';
+
+const name = readlineSync.question('Your nickname?');
+console.log ('Hello,' + name + '!');
