bd71ff55331964c45919eee87ca1bd6f4f4c2d29
Quiet_Death <familuuuuudamira@gmail.com>
2025-03-06 11:42:51 +0300

    6
    mda yzh

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,6 @@
 console.log ('Weclome to the Brain Games!');
 import readlineSync from 'readline-sync';
+import name from '../bin/brain-even'
 
 const name = readlineSync.question('Your nickname?');
 console.log ('Hello,' + name + '!');
